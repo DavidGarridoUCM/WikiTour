dd47111226db059663dc2c29c5ea9e32465b04b3
DavidGarridoUCM <74601213+DavidGarridoUCM@users.noreply.github.com>
2024-03-14 18:36:52 +0100

    .

--- API/src/controller/usuario.js
+++ API/src/controller/usuario.js
@@ -3,8 +3,29 @@
 var Usuario = require('../models/usuario');
  
 const createUsuario = async (req, res)=>{
-       const usu = await Usuario.create(req.body);
+       try{
+              const usu = await Usuario.create(req.body);
+              res.status(200).json(usu);
+       }
+       catch (err){
+              console.log(err.message);
+       }     
+}
+
+/*const deleteUsuario = async (req, res)=>{
        res.status(200).json(usu)
 }
+
+const updateUsuario = async (req, res)=>{
+       res.status(200).json(usu)
+}
+
+const getUsuario = async (req, res)=>{
+       res.status(200).json(usu)
+}
+
+const getUsuarios = async (req, res)=>{
+       res.status(200).json(usu)
+}*/
 
 module.exports = {createUsuario};
